b3060628ba20c217cb49340fc99934d7cef9c036
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2024-04-24 10:57:47 -0400

    updated methods
    changing methods to help with code flow

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -10,15 +10,12 @@ class Game:
         self.phrases = ["The Way of the Kings", "Words of Radiance", "Oathbringer", "Rhythm of war", "Edgedance", "Dawnshard", "The Final Empire", "Ther Well of Ascension", "The Hero of Ages", "Secret History", "The Alloy of Law", "Shadows of Self", "The Bands of Mourning", "The Lost Metal", "Tress of the Emerald Sea", "Yumi and The Nightmare Painter", "The Sunlit Man", "Elantris", "Warbreaker"]
 
 
-    def welcome(self):
-        print("""
-========================
-Welcome to Phrase Hunter
-========================
+    def set_phrase(self):
+        self.game_phrase = Phrase(random.choice(self.phrases))
 
-Please choose a single letter.
-You will have 5 wrong guesses till the game ends.
-    """
+
+    def welcome(self):
+        print(f"""========================\nWelcome to Phrase Hunter\n========================\n\nPlease choose a single letter.\nYou will have {self.max_tries} wrong guesses till the game ends."""
         )
         input("Press enter to continue...")
         self.set_phrase()
@@ -28,71 +25,56 @@ You will have 5 wrong guesses till the game ends.
         self.game_phrase.phrase_reveal(self.guessed_letters)
 
 
-    def set_phrase(self):
-        self.game_phrase = Phrase(random.choice(self.phrases))
-
-
     def display_game_info(self):
-        print(f"Your guesses so far: {" ".join(self.guessed_letters)}")
         print(f"You have {self.max_tries - self.wrong_tries} remaining attemp(s).\n")
 
+    
+    def check_guesses(self):
+        self.game_phrase.phrase_reveal(self.guessed_letters)
 
-    def guess(self):
-        self.game_loss()
-        self.display_game_info()
-        guess = input("Please guess a letter you think is in the phrase:  ")
-        while not guess.isalpha() or len(guess) != 1 or guess in self.guessed_letters:
-            self.game_loss()
-            if guess in self.guessed_letters:
+
+    def user_guess(self):
+        while self.game_phrase.hidden_phrase != self.game_phrase.phrase and self.wrong_tries != self.max_tries:
+            print(self.game_phrase.hidden_phrase)
+            print(self.game_phrase)
+            user_guess = input("Please select a letter to guess:    ")
+            if user_guess not in self.game_phrase.phrase:
                 self.wrong_tries += 1
-                self.display_game_info()
-                print("You have already guessed that letter.\n")
-                guess = input("Please guess a letter you think is in the phrase:  ")
-            elif not self.warning:
-                self.display_game_info()
-                print("Sorry that is not a single letter.\n")
-                print("You are allowed one mistype, next will cost an attempt\n")
-                self.warning = True
-                guess = input("Please guess a letter you think is in the phrase:  ")
-            else:
-                self.wrong_tries += 1
-                self.display_game_info()
-                print("Sorry that is not a single letter.\n")
-                guess = input("Please guess a letter you think is in the phrase:  ")
-        self.guessed_letters.append(guess.lower())
-        if guess not in self.game_phrase.phrase:
-            self.wrong_tries += 1
-
-
-    def check_guess(self):
-        while self.game_phrase.phrase != self.game_phrase.hidden_phrase:
-            self.game_loss()
-            self.guess()
-            self.game_phrase.phrase_reveal(self.guessed_letters)
+            self.guessed_letters.append(user_guess)
+            self.check_guesses()
 
 
     def game_loss(self):
         if self.wrong_tries == self.max_tries:
-            print(f"You ran out of attempts.")
-            self.play_again()
+            total_right = 0
+            for letter in self.game_phrase.hidden_phrase:
+                if letter.isalpha():
+                    total_right += 1
+            print(f"You did not guess correct\nThe phrase was {self.game_phrase.phrase}\nYou got {total_right} letters right.")
+
+
+    def game_win(self):
+        if self.game_phrase.hidden_phrase == self.game_phrase.phrase:
+            print("Congrats you won")
+
+        self.play_again()
+
 
 
     def play_again(self):
-        user_input = input("Press '1' to playa gain\nPress '2' to quit.")
-        if user_input == 1:
+        choice = input("Enter '1' to play again\nEnter '2' to stop playing.")
+        if choice == 1:
             self.run_game()
-        elif user_input == 2:
-            quit()
-        
-        print(user_input)    
-        if user_input == 1:
-            self.welcome()
-        elif user_input == 2:
-            quit()
+        elif choice == 2:
+            exit()
+            
+        self.play_again()
 
     def run_game(self):
         self.welcome()
-        self.check_guess()
+        self.user_guess()
+        self.game_loss()
+        self.game_win()
 
 
 
